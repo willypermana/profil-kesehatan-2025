c6c70e9644e45e16b241887fe303eadc9f3c1ab7
willypermana <willypermana@gmail.com>
2026-07-07 15:15:47 +0700

    20260707 dell commit

--- bab_07/bab_07_01a_SKAMRT.py
+++ bab_07/bab_07_01a_SKAMRT.py
@@ -8,23 +8,22 @@ locale.setlocale(locale.LC_ALL, 'id_ID.UTF8')
 
 import sys, os, inspect
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-# run this script from any directory
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
 
-berkasData = currentdir +'\\bab_07_04_dataTFUIKL.csv'
-berkasSimpan = currentdir +'\\bab_07_04_TFUIKL.pdf'
-# judulDiagram = 'Cakupan TFU IKL\nTahun 2022'
+berkasData = currentdir +'\\bab_07_01a_dataSKAMRT.csv'
+berkasSimpan = currentdir +'\\bab_07_01a_SKAMRT.pdf'
+# judulDiagram = 'Cakupan SKAMRT\nTahun 2023'
 sumbuX = 'Cakupan'
-sumbuY = 'Kecamatan/ Kabupaten'
-labelBar1 = 'TFU dilakukan IKL dan memenuhi syarat'
+sumbuY = 'Kecamatan\ Kabupaten'
+labelBar1 = 'Sampel Air Minum Memenuhi Syarat'
 tickerSumbuX = np.arange(0,110,25)
 
 # read data file
-colnames = ['kecamatan','TFU']
+colnames = ['kecamatan','airMinumMS']
 data = pandas.read_csv(berkasData, names=colnames, sep=';')
 kecamatan = data.kecamatan.tolist()
-bar1 = data.TFU.tolist()
+bar1 = data.airMinumMS.tolist()
 
 ind = np.arange(len(kecamatan))  # the x locations for the groups
 width = 0.5       # the width of the bars
@@ -37,16 +36,16 @@ rects1 = ax.barh(ind, bar1, width, color='steelblue', label=labelBar1)
 # ax.set_title(judulDiagram)
 ax.set_xlabel(sumbuX)
 ax.set_ylabel(sumbuY)
-
-ax.set_xticks(tickerSumbuX)
 formatter = FuncFormatter(lambda x, pos: "{:n}%".format(x))
 ax.xaxis.set_major_formatter(formatter)
+ax.set_xticks(tickerSumbuX)
 
 ax.set_yticks(ind)
 ax.set_yticklabels(list([ '\n'.join(wrap(l, 10)) for l in kecamatan ]))
 ax.invert_yaxis()
 
 ax.tick_params(axis='both', which='major', labelsize='small')
+#ax.grid(axis='y',ls='dashed')
 ax.spines['top'].set_visible(False)
 ax.spines['right'].set_visible(False)
 
@@ -55,18 +54,14 @@ box = ax.get_position()
 ax.set_position([box.x0, box.y0 + box.height * 0.1, box.width, box.height * 0.9])
 ax.legend(fontsize='x-small', loc='upper center', bbox_to_anchor=(0.5, -0.15), fancybox=True, shadow=True, ncol=1)
 
-# add data label
+# add data labels
 for i, v in enumerate(bar1):
     ax.text(v+0.5, i, locale.format_string("%.2f", v), ha='left', va='center', fontsize='x-small')
-
+    
 # finishing
 pyrfig = plt.figure(1)
 pyrfig.set_figwidth(8)
 pyrfig.set_figheight(5)
-# tight_layout to make consistent size
-# adjust subplot to make room for legend
-fig.subplots_adjust(bottom=-0.15)
-plt.tight_layout()
 fig.savefig(berkasSimpan, bbox_inches='tight')
 plt.close(pyrfig)
 #plt.show()
